c23b7bb3fec163e6626375cdc2138a280f17423e
crea <654321su@gmail.com>
2018-05-23 21:00:29 +0900

    #045 Publication and Subscription Review

--- employees/client/componenets/employee_list.js
+++ employees/client/componenets/employee_list.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 
-const EmployeeList = () => {
+const EmployeeList = (props) => {
+    // props.employees => an array of employee objects
+
     return (
         <div>
             <div className="employee-list">
@@ -17,5 +19,5 @@ export default createContainer(() => {
     Meteor.subscribe('employees');
 
     // return an object. Whatever we return will be sent to EmployeeList as props
-    return { employees: Employees.find({}).fetch };
+    return { employees: Employees.find({}).fetch() };
 }, EmployeeList);
