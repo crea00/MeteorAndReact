63eff9d02a594ecd1a136683c76094d4da9e555e
crea <654321su@gmail.com>
2018-05-23 17:47:40 +0900

    #024 Implementing State

--- images/client/main.js
+++ images/client/main.js
@@ -8,13 +8,22 @@ import ImageList from './components/image_list';
 
 // Create a component
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { images: [] };
+  }
+
   componentWillMount() {
     // Fantastic place to load data!
     axios.get('https://api.imgur.com/3/gallery/hot/viral/0.json')
-      .then(response => console.log(response));
+      .then(response => this.setState({ images: response.data.data }));
+    // NEVER DO THIS -
+    // this.state.images = [ {}, {} ];
   }
 
   render() {
+    console.log(this.state.images);
     return (
       <div>
         <ImageList />
@@ -26,5 +35,4 @@ class App extends Component {
 // Render this component to the screen
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
-
 });
