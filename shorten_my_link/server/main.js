37196e2c419a16194b85c4734eb182b138ffccd1
crea <654321su@gmail.com>
2018-05-25 15:35:34 +0900

    #072 Redirecting Users to Long Links

--- shorten_my_link/server/main.js
+++ shorten_my_link/server/main.js
@@ -9,10 +9,20 @@ Meteor.startup(() => {
   });
 });
 
+// Executed whenever a user visits with a route like 'localhost:3000/abcd'
+function onRoute(req, res, next) {
+  // Take the token out of the url and try to a find a matching link in The Links Collection
+  const link = Links.findOne({ token: req.params.token });
+  
+  // If we find a link object, redirect the user to the long URL
+
+  // If we don't find a link object, send the user to our normal React app
+  
+}
+
 //    localhost:3000/   NO MATCH
 //    localhost:3000/books/harry_potter   NO MATCH
 //    localhost:3000/abcd   will match!!
-
 const middleware = ConnectRoute(function(router) {
   router.get('/:token', (req) => console.log(req));
 });
