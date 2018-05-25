9d8c09fb985c0638b08fecbf48e0e232e3d94326
crea <654321su@gmail.com>
2018-05-25 13:29:15 +0900

    #071 Finding Tokens in URLs

--- shorten_my_link/server/main.js
+++ shorten_my_link/server/main.js
@@ -1,8 +1,20 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
 
 Meteor.startup(() => {
   Meteor.publish('links', function() {
     return Links.find({});
-  })
+  });
 });
+
+//    localhost:3000/   NO MATCH
+//    localhost:3000/books/harry_potter   NO MATCH
+//    localhost:3000/abcd   will match!!
+
+const middleware = ConnectRoute(function(router) {
+  router.get('/:token', (req) => console.log(req));
+});
+
+WebApp.connectHandlers.use(middleware);
