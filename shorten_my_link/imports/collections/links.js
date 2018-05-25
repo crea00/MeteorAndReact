e372c201ceece8af76d5e86817b82ee61017b7f3
crea <654321su@gmail.com>
2018-05-25 11:45:00 +0900

    #067 Generating Unique Tokens for URLs

--- shorten_my_link/imports/collections/links.js
+++ shorten_my_link/imports/collections/links.js
@@ -5,6 +5,10 @@ import { check, Match } from 'meteor/check';
 Meteor.methods({
     'links.insert': function(url) {
         check(url, Match.Where(url => validUrl.is_uri(url)));
+
+        // We're ready to save the Url
+        const token = Math.random().toString(36).slice(-5);
+        Links.insert({ url, token, clicks: 0 });
     }
 });
 
