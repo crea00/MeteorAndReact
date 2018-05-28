ebdf10fb14d240ef494a29f4e4fcb618649d53cf
crea <654321su@gmail.com>
2018-05-28 16:55:38 +0900

    #090 Routing with React Router

--- markbin/client/main.js
+++ markbin/client/main.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// react-router -> react-router-dom
+import { BrowserRouter as Router, Route, IndexRoute, browserHistory } from 'react-router-dom';
 
 import App from './components/app';
 import { Bins } from '../import/collections/bins';
 
+// the Key idea to remember is that react router displays different sets of components based on the current url
+const routes = (
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+        </Route>
+    </Router>
+);
+
 Meteor.startup(() => {
-    ReactDOM.render(<App />, document.querySelector('.render-target'));
+    ReactDOM.render(routes, document.querySelector('.render-target'));
 });
