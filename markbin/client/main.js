4fcc81004467f20aeba5ee7c5822902fdcf66cd8
crea <654321su@gmail.com>
2018-05-28 17:46:39 +0900

    #091 More on React Router, Change React Router v3.2.0

--- markbin/client/main.js
+++ markbin/client/main.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// react-router -> react-router-dom
-import { BrowserRouter as Router, Route, IndexRoute, browserHistory } from 'react-router-dom';
+// react-router -> react-router-dom, Router -> BrowserRouter as Router in React-Router 4
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
+import BinsMain from './components/bins/bins_main';
+import BinsList from './components/bins/bins_list';
 import { Bins } from '../import/collections/bins';
 
 // the Key idea to remember is that react router displays different sets of components based on the current url
 const routes = (
     <Router history={browserHistory}>
         <Route path="/" component={App}>
+            <Route path="/bins" component={BinsMain} />
         </Route>
     </Router>
 );
