1b8c025f1d462fc0d1df650b4cc3b2d4d814260b
crea <654321su@gmail.com>
2018-05-28 15:18:10 +0900

    #084 Insert Bins Method

--- markbin/import/collections/bins.js
+++ markbin/import/collections/bins.js
@@ -1,3 +1,15 @@
 import { Mongo } from 'meteor/mongo';
 
+Meteor.methods({
+    'bins.insert': function() {
+        return Bins.insert({
+            createdAt: new Date(),
+            content: '',
+            sharedWith: [],
+            // If we don't use function keyword, it would actually break this.userId
+            ownerId: this.userId
+        });
+    }
+});
+
 export const Bins = new Mongo.Collection('bins');
